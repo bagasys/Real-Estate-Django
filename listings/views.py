13aaac5a25e4b31cb54a87080872766312fb9781
Bagas Yanuar Sudrajad <37441075+bagasys@users.noreply.github.com>
2020-01-09 22:15:59 +0700

    search listings

--- listings/views.py
+++ listings/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
-
+from .choices import price_choices, bedroom_choices, state_choices
 from .models import Listing
 
 
@@ -27,4 +27,40 @@ def listing(request, listing_id):
 
 
 def search(request):
-  return render(request, 'listings/search.html')
+  queryset_list = Listing.objects.order_by('-list_date')
+
+  if 'keywords' in request.GET:
+    keywords = request.GET['keywords']
+    if keywords:
+      queryset_list = queryset_list.filter(description__icontains=keywords)
+
+
+  if 'city' in request.GET:
+    city = request.GET['city']
+    if city:
+      queryset_list = queryset_list.filter(city__iexact=city)
+
+
+  if 'state' in request.GET:
+    state = request.GET['state']
+    if state:
+      queryset_list = queryset_list.filter(state__iexact=state)
+  
+  if 'bedrooms' in request.GET:
+    bedrooms = request.GET['bedrooms']
+    if bedrooms:
+      queryset_list = queryset_list.filter(bedrooms__gte=bedrooms)
+  
+  if 'price' in request.GET:
+    price = request.GET['price']
+    if price:
+      queryset_list = queryset_list.filter(price__lte=price)
+
+  context = {
+    'listings': queryset_list,
+    'price_choices': price_choices,
+    'state_choices': state_choices,
+    'bedroom_choices': bedroom_choices,
+    'values': request.GET
+  }
+  return render(request, 'listings/search.html', context)
