543fd1f6be619f286d73e0e25b2b869b3b6b47bf
Bagas Yanuar Sudrajad <37441075+bagasys@users.noreply.github.com>
2020-01-12 21:48:19 +0700

    remove email from settings

--- btre/settings.py
+++ btre/settings.py
@@ -143,15 +143,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-
-# mail
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = True
-
-
 try:
     from .local_settings import *
 except ImportError:
